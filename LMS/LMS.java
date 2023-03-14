a10f1c9ddc49662ed1ebc84b7695fa875d6be21f
Casey Fainberg <caseyfainberg@Caseys-MacBook-Pro-2.local>
2023-03-14 12:24:09 -0400

    added LMS class stub

---
+++ LMS/LMS.java
@@ -0,0 +1,76 @@
+package LMS;
+public class LMS {
+  private User user;
+  private Course currCourse;
+  private Module currModule;
+  private Lesson currLesson;
+  private CourseList courseList;
+  private UserList userList;
+
+  public LMS() {
+
+  }
+  public boolean login(String username, String password) {
+    return true;
+  }
+  public void logout() {
+    return;
+  }
+  public void addUser(String firstName, String lastName, String email, String username, String password) {
+    return;
+  }
+  public void addCourse(String title, Langauge Topic, String description, ArrayList<Module> modules, Difficulty difficulty) {
+    return;
+  }
+  public ArrayList<Course> findCourses(String keyword) {
+    return null;
+  }
+  public ArrayList<Course> getAllCourses() {
+    return null;
+  }
+  public ArrayList<Course> getRegisteredCourses() {
+    return null;
+  }
+  public ArrayList<Course> getCreatedCourses() {
+    return null;
+  }
+  public ArrayList<Course> getCompletedCourses() {
+    return null;
+  }
+  public boolean checkUsername(String username) {
+    return true;
+  }
+  public boolean checkCourseTitle(String title) {
+    return true;
+  }
+  public Assessment getModuleQuiz() {
+    return null;
+  }
+  public void evaluateAssessment(Assessment assessment, ArrayList<int> answers) {
+    return;
+  }
+  public void updateGrade(UUID courseid, String moduleName, double grade) {
+    return;
+  }
+  public void addCourseComment(String decription) {
+    return;
+  }
+  public void addModuleComment(String decription) {
+    return;
+  }
+  public ArrayList<Comment> getCourseComments() {
+    return null;
+  }
+  public ArrayList<Comment> getModuleComments() {
+    return null;
+  }
+  public void addReply(Comment comment, String decription) {
+    return;
+  }
+  public void addReview(String description, int rating) {
+    return;
+  }
+  public ArrayList<Review> getReviews() {
+    return null;
+  }
+}
