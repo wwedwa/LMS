dbe8657d534799ac78e0689ccec28cc82c6d89ce
William Edwards <wwall@Williams-MacBook-Air.local>
2023-03-15 10:58:43 -0400

    Updated data reader/writer classes

--- DataLoader.java
+++ DataLoader.java
@@ -1,5 +1,8 @@
 package LMS;
+
 import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.UUID;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
@@ -11,6 +14,34 @@ public class DataLoader extends DataConstants {
   }
 
   public static ArrayList<User> LoadUsers() {
-    JSONParser parser = new JSONParser();
+		ArrayList<User> users = new ArrayList<User>();
+		
+		try {
+			FileReader reader = new FileReader(USER_FILE_NAME);
+			JSONArray peopleJSON = (JSONArray)new JSONParser().parse(reader);
+			
+			for(int i = 0; i < peopleJSON.size(); ++i) {
+				JSONObject personJSON = (JSONObject)peopleJSON.get(i);
+				UUID id = UUID.fromString((String)personJSON.get(USER_ID));
+				String username = (String)personJSON.get(USERNAME);
+        String password = (String)personJSON.get(PASSWORD);
+				String firstName = (String)personJSON.get(FIRST_NAME);
+				String lastName = (String)personJSON.get(LAST_NAME);
+				String email = (String)personJSON.get(EMAIL);
+				String type = (String)personJSON.get(TYPE);
+
+        if (type.equals("student")) {
+				  users.add(new User(firstName, lastName, email, username, password, type, id));
+        } else if (type.equals("author")) {
+          users.add(new Author(firstName, lastName, email, username, password, type, id));
+        }
+			}
+			
+			return users;
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return null;
   }
 }
--- DataWriter.java
+++ DataWriter.java
@@ -1,16 +1,43 @@
 package LMS;
 
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
-import org.json.simple.parser.JSONParser;
 
 public class DataWriter extends DataConstants {
   
-  public static void SaveCourses(ArrayList<Course> courses) {
+  public static void saveCourses(ArrayList<Course> courses) {
     
   }
 
-  public static void SaveUsers(ArrayList<User>) {
+	public static void saveUsers() {
+		UserList users = UserList.getInstance();
+		ArrayList<User> userList = users.getUsers();
+		JSONArray jsonUsers = new JSONArray();
+		
+		for (int i = 0; i < userList.size(); ++i) {
+			jsonUsers.add(getUserJSON(userList.get(i)));
+		}
 
-  }
+    try (FileWriter file = new FileWriter(USER_FILE_NAME)) {
+      file.write(jsonUsers.toJSONString());
+      file.flush();
+    } catch (IOException e) {
+        e.printStackTrace();
+    }
+	}
+	
+	public static JSONObject getUserJSON(User user) {
+		JSONObject userDetails = new JSONObject();
+		userDetails.put(USER_ID, user.getId().toString());
+		userDetails.put(USERNAME, user.getUsername());
+    userDetails.put(PASSWORD, user.getPassword());
+		userDetails.put(FIRST_NAME, user.getFirstName());
+		userDetails.put(LAST_NAME, user.getLastName());
+		userDetails.put(EMAIL, user.getEmail());
+    userDetails.put(TYPE, user.getType());
+    return userDetails;
+	}
 }
