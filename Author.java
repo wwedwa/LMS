994348ecb647008800a2d0f33289fd8ab0ee0851
William Edwards <wwall@Williams-MacBook-Air.local>
2023-03-15 10:56:49 -0400

    Created constructors, getters

--- Author.java
+++ Author.java
@@ -1,12 +1,20 @@
-import java.util.ArrayList;
+package LMS;
 
-public class Author {
+import java.util.ArrayList;
+import java.util.UUID;
+
+public class Author extends User {
     private ArrayList<Course> createdCourses;
     private double rating;
 
-    public Author(String firstName, String lastName, String email, String username, String password) {
-
+    public Author(String firstName, String lastName, String email, String username, String password, String type) {
+      super(firstName, lastName, email, username, password, type);
     }
+
+    public Author(String firstName, String lastName, String email, String username, String password, String type, UUID id) {
+      super(firstName, lastName, email, username, password, type, id);
+    }
+
     public void updateRating(int rating) {
         
     }
