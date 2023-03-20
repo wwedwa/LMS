ec358d7b2e08832dbf325fef73404352007034d1
William Edwards <wwall@Williams-MacBook-Air.local>
2023-03-19 22:16:47 -0400

    Fixed constructor

--- src/Lesson.java
+++ src/Lesson.java
@@ -3,7 +3,7 @@ public class Lesson {
     private String title;
     private String content;
 
-    public Lesson(String title, String content, String example) {
+    public Lesson(String title, String content) {
         this.title = title;
         this.content = content;
     }
