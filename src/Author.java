21b30f717ab3dc43419ceee7f9d1c5734a79b52d
ParkerNoah <parker.h.noah@gmail.com>
2023-03-23 12:21:34 -0400

    added some javadoc

--- src/Author.java
+++ src/Author.java
@@ -10,19 +10,40 @@ import java.util.UUID;
 public class Author extends User {
     private ArrayList<Course> createdCourses;
     private double rating;
-
+    /**
+    * Author constructor with type
+    *@param firstName
+    *@param lastName
+    *@param email
+    *@param username
+    *@param password
+    *@param type
+    */
     public Author(String firstName, String lastName, String email, String username, String password, String type) {
       super(username, firstName, lastName, email, password);
     }
-
+    /**
+    * Author constructor with id
+    *@param firstName
+    *@param lastName
+    *@param email
+    *@param username
+    *@param password
+    *@param id
+    */
     public Author(String firstName, String lastName, String email, String username, String password, UUID id) {
       super(username, firstName, lastName, email, password, id);
     }
-
+    /**
+    * update rating
+    *@param rating
+    */
     public void updateRating(int rating) {
         this.rating = rating;
     }
-
+    /**
+    * returns all created courses
+    */
     public ArrayList<Course> getCreatedCourses() {
         return this.createdCourses;
     }
