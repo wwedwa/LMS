8fd057abd85efd006a000794ce32177e20768c65
ParkerNoah <parker.h.noah@gmail.com>
2023-03-23 12:23:54 -0400

    added some javadoc

--- src/Comment.java
+++ src/Comment.java
@@ -12,16 +12,30 @@ public class Comment {
     private UUID commentid;
     private String description;
     private ArrayList<Comment> replies;
-
+    /**
+    * Comment constructor with replies
+    *@param author
+    *@param description
+    *@param replies
+    */
     public Comment(User author, String description, ArrayList<Comment> replies) {
         this.author = author;
         this.description = description;
         this.replies = replies;
     }
+    /**
+    * Comment constructor
+    *@param author
+    *@param description
+    */
     public Comment(User author, String description) {
         this.author = author;
         this.description = description;
     }
+    /**
+    * add reply comment
+    *@param comment
+    */
     public void addReply(Comment comment) {
         replies.add(comment);
     }
