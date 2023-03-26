a85a38e4319fa549dd8f5ca0e2b13491625763e7
William Edwards <wwall@Williams-MacBook-Air.local>
2023-03-26 11:17:10 -0400

    Fixed formatting

--- src/Author.java
+++ src/Author.java
@@ -8,45 +8,49 @@ import java.util.UUID;
  * @author The Lobsters
  */
 public class Author extends User {
-    private ArrayList<Course> createdCourses;
-    private double rating;
-    /**
-    * Author constructor with type
-    *@param firstName
-    *@param lastName
-    *@param email
-    *@param username
-    *@param password
-    *@param type
-    */
-    public Author(String username, String firstName, String lastName, String email, String password) {
-      super(username, firstName, lastName, email, password);
-      createdCourses = new ArrayList<Course>();
-      setType("author");
-    }
-    /**
-    * Author constructor with id
-    *@param firstName
-    *@param lastName
-    *@param email
-    *@param username
-    *@param password
-    *@param id
-    */
-    public Author(String firstName, String lastName, String email, String username, String password, UUID id) {
-      super(username, firstName, lastName, email, password, id);
-    }
-    /**
-    * update rating
-    *@param rating
-    */
-    public void updateRating(int rating) {
-        this.rating = rating;
-    }
-    /**
-    * returns all created courses
-    */
-    public ArrayList<Course> getCreatedCourses() {
-        return this.createdCourses;
-    }
+  private ArrayList<Course> createdCourses;
+  private double rating;
+
+  /**
+  * Author constructor with type
+  *@param firstName
+  *@param lastName
+  *@param email
+  *@param username
+  *@param password
+  *@param type
+  */
+  public Author(String username, String firstName, String lastName, String email, String password) {
+    super(username, firstName, lastName, email, password);
+    createdCourses = new ArrayList<Course>();
+    setType("author");
+  }
+
+  /**
+  * Author constructor with id
+  *@param firstName
+  *@param lastName
+  *@param email
+  *@param username
+  *@param password
+  *@param id
+  */
+  public Author(String firstName, String lastName, String email, String username, String password, UUID id) {
+    super(username, firstName, lastName, email, password, id);
+  }
+
+  /**
+  * update rating
+  *@param rating
+  */
+  public void updateRating(int rating) {
+    this.rating = rating;
+  }
+
+  /**
+  * returns all created courses
+  */
+  public ArrayList<Course> getCreatedCourses() {
+    return this.createdCourses;
+  }
 }
