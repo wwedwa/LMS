7aecbef929f0cc79267a342727bced486ac1a627
wwedwa <w.wallace.edwards@gmail.com>
2023-03-30 12:29:38 -0400

    Uncommented clearScreen()

--- src/LMSUI.java
+++ src/LMSUI.java
@@ -559,8 +559,8 @@ public class LMSUI {
   }
 
   private void clearScreen() {
-    //System.out.print("\033[H\033[2J");
-    //System.out.flush();
+    System.out.print("\033[H\033[2J");
+    System.out.flush();
   }
 
   private void sleep(int milliseconds) {
